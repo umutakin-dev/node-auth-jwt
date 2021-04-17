8b82ef638a20ef5fc92bd7dc4ff403d874b6b867
Umut Akin <umutakin@fastmail.com>
2021-04-18 02:07:08 +0300

    node-auth-jwt 09 - cookies primer

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // middleware
 app.use(express.static("public"));
 app.use(express.json()); // for parsing json requests
+app.use(cookieParser());
 
 // view engine
 app.set("view engine", "ejs");
@@ -27,3 +29,20 @@ mongoose
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
 app.use(authRoutes);
+
+//cookies
+app.get('/set-cookies', (req, res) => {
+    //res.setHeader('Set-Cookie', 'newUser=true');
+    res.cookie('newUser', false);
+    res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpsOnly: true });
+    res.send('you got the cookies!');
+});
+
+app.get('/read-cookies', (req, res) => {
+    const cookies = req.cookies;
+
+    console.log(cookies);
+    console.log(cookies.newUser);
+
+    res.json(cookies);
+});
